8af7a316572686c7be3c9721d7006b2470c236f8
antoniolg <antonio@devexperto.com>
2024-10-17 13:20:53 +0200

    Use CMP Resources

--- composeApp/src/commonMain/kotlin/io/devexpert/kmptraining/ui/screens/login/Login.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/kmptraining/ui/screens/login/Login.kt
@@ -12,13 +12,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.saveable.rememberSaveable
+import kmptraining.composeapp.generated.resources.Res
+import kmptraining.composeapp.generated.resources.login_button
+import kmptraining.composeapp.generated.resources.login_password
+import kmptraining.composeapp.generated.resources.login_username
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun Login() {
@@ -48,19 +52,19 @@ fun LoginForm(modifier: Modifier = Modifier) {
         OutlinedTextField(
             value = username,
             onValueChange = { username = it },
-            label = { Text("Username") }
+            label = { Text(stringResource(Res.string.login_username)) }
         )
         OutlinedTextField(
             value = password,
             onValueChange = { password = it },
-            label = { Text("Password") },
+            label = { Text(stringResource(Res.string.login_password)) },
             visualTransformation = PasswordVisualTransformation()
         )
         Button(
             onClick = { username = ""; password = "" },
             enabled = isLoginEnabled
         ) {
-            Text("Login")
+            Text(stringResource(Res.string.login_button))
         }
     }
 }
