6bb578a8ded2644ba461641941d3e6c97021857b
antoniolg <antonio@devexperto.com>
2024-10-17 13:12:02 +0200

    Add state to login form

--- composeApp/src/commonMain/kotlin/io/devexpert/kmptraining/ui/screens/login/Login.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/kmptraining/ui/screens/login/Login.kt
@@ -10,8 +10,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -30,14 +35,30 @@ fun Login() {
 
 @Composable
 fun LoginForm(modifier: Modifier = Modifier) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val isLoginEnabled = username.isNotBlank() && password.isNotBlank()
+
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        OutlinedTextField(value = "user", onValueChange = {})
-        OutlinedTextField(value = "password", onValueChange = {})
-        Button(onClick = { }) {
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") }
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(
+            onClick = { username = ""; password = "" },
+            enabled = isLoginEnabled
+        ) {
             Text("Login")
         }
     }
