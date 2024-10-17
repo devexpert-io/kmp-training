95794b7dde9bbb86af4de84edf88ab96ecb8a115
antoniolg <antonio@devexperto.com>
2024-10-17 12:16:12 +0200

    Create button that prints in console

--- composeApp/src/commonMain/kotlin/io/devexpert/kmptraining/App.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/kmptraining/App.kt
@@ -1,22 +1,22 @@
 package io.devexpert.kmptraining
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
-import io.devexpert.kmptraining.data.NotesRepository
-import io.devexpert.kmptraining.domain.Note
-import io.devexpert.kmptraining.ui.screens.notes.Notes
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        val notes by produceState(initialValue = emptyList<Note>()) {
-            value = NotesRepository().getNotes()
-        }
+        MyButton("Click Me")
+    }
+}
 
-        Notes(notes)
+@Composable
+fun MyButton(text: String) {
+    Button(onClick = { println("Button Clicked") }) {
+        Text(text = text)
     }
 }
