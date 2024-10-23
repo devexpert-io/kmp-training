2b1446838db771c6ba1d39e452b6dc46ebf560ed
antoniolg <antonio@devexperto.com>
2024-10-23 13:09:00 +0200

    Exercise: Use animated visibility on login form

--- composeApp/src/commonMain/kotlin/io/devexpert/kmptraining/ui/screens/login/Login.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/kmptraining/ui/screens/login/Login.kt
@@ -1,11 +1,11 @@
 package io.devexpert.kmptraining.ui.screens.login
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -94,14 +94,22 @@ fun LoginForm(
             onValueChange = { password = it },
             onDone = { if (isLoginEnabled) onLoginClick(username, password) }
         )
-        StyledButton(
-            onClick = { onLoginClick(username, password) },
-            enabled = isLoginEnabled
-        ) {
-            Text(stringResource(Res.string.login_button))
+
+        AnimatedVisibility(isLoginEnabled) {
+            StyledButton(
+                onClick = { onLoginClick(username, password) }
+            ) {
+                Text(stringResource(Res.string.login_button))
+            }
         }
-        errorMessage?.let {
-            Text(it, color = MaterialTheme.colorScheme.error)
+
+        AnimatedVisibility(errorMessage != null) {
+            if (errorMessage != null) {
+                Text(
+                    text = errorMessage,
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
         }
     }
 }
